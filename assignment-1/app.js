c08c3e23e8c21bfbd2f2c84ad51261722e0c5067
rutvik-patel8 <98960675+rutvik-patel8@users.noreply.github.com>
2022-02-14 11:46:49 +0530

    add assignment-1

---
+++ assignment-1/app.js
@@ -0,0 +1,100 @@
+const App = Vue.createApp({
+    data(){
+        return{
+            colorval : 'white',
+            textcolor : 'black'
+        }
+    },
+    methods: {
+        col(){
+            let max = this.logs().length
+            console.log(max)
+            return max;
+        },
+        updateColor(event){
+            let color = event.target.value
+            if(color == 'red' || color == 'green' || color == 'blue'){
+                this.textcolor = 'white';
+            }
+            else{
+                this.textcolor = 'black';
+            }
+            this.colorval = color; 
+            console.log(this.colorval)
+
+        }
+        
+    },
+    computed:{
+       logs(){
+         l = [
+            {
+                date : '2022-02-01',
+                day : 'Monday',
+                section : 1,
+                gitRepo : 'https://github.com/',
+                pregress: 80,
+                topics : ['overvieew' , 'lifecycle' , 'events']
+            },
+            {
+                date : '2022-02-02',
+                day : 'Tuesday',
+                section : 2,
+                gitRepo : 'https://github.com/',
+                pregress: 40,
+                topics : ['mehtod','computed','events','watchs','propes','styling']
+            },
+            {
+                date : '2022-02-03',
+                day : 'Wednesday',
+                section : 3,
+                gitRepo : 'https://github.com/',
+                pregress: 10,
+                topics : ['overvieew','lifecycle','events','watchs','propes','styling','props']
+            },
+        ]
+        return l
+       } ,
+       col2(){
+        l = [
+            {
+                date : '2022-02-01',
+                day : 'Monday',
+                section : 1,
+                gitRepo : 'https://github.com/',
+                pregress: 80,
+                topics : ['overvieew' , 'lifecycle' , 'events']
+            },
+            {
+                date : '2022-02-02',
+                day : 'Tuesday',
+                section : 2,
+                gitRepo : 'https://github.com/',
+                pregress: 40,
+                topics : ['mehtod','computed','events','watchs','propes','styling']
+            },
+            {
+                date : '2022-02-03',
+                day : 'Wednesday',
+                section : 3,
+                gitRepo : 'https://github.com/',
+                pregress: 10,
+                topics : ['overvieew','lifecycle','events','watchs','propes','styling','props']
+            },
+        ]
+           let max2=0;      
+           for(let i=0;i<l.length;i++){
+               
+               if(l[i].topics.length>max2){
+                   max2 = l[i].topics.length
+                   
+               }   
+           }
+          return max2
+       }
+       
+       
+    }
+});
+
+App.mount('#app')
